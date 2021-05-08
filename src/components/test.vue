e8198d10f69718542a133b9bd9f6be228da687a7
cherotich <carolcaro60@gmail.com>
2021-05-08 15:45:55 +0300

    Added the country states, the clear button

--- src/components/test.vue
+++ src/components/test.vue
@@ -50,3 +50,65 @@ export default {
     }
     }
 </script>
+
+
+
+<template>
+  <main v-if="!loading">
+  <DataTitle :text="title" :dataDate="dataDate"/>
+  <!-- <DataBoxes :stats="stats"/> -->
+  <CountrySelect @get-country="getCountryData" :countries="countries" />
+  </main>
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+ <div class ="text-gray-500 text-3xl mt-10 mb-6">
+   Fetching data
+ </div>
+ <img :src="loadingImage" class="w-24 m-auto" alt=""/>
+  </main>
+</template>
+
+<script>
+import DataTitle from '@/components/DataTitle'
+import DataBoxes from '@/components/DataBoxes'
+import CountrySelect from '@/components/CountrySelect'
+
+// import DataTitle from '../components/DataTitle.vue'
+export default {
+  name: 'Home',
+  components: { 
+    DataTitle,
+    DataBoxes,
+    CountrySelect
+   },
+    DataTitle() {
+    return {
+      loading:true,
+      title:'Clobal',
+      dataDate:'',
+      stats:{},
+      countries:[],
+      loadingImage:require('../assets/hourglass.gif'),
+
+    }
+    },
+  methods:{
+  async  fetchCovidData() {
+    const res = await fetch('https://api.covid19api.com/summary')
+    const data = await res.json()
+    return data
+      },
+      getCountryData(country) {
+
+        }
+    },
+ async created(){
+    const data  = await  this.fetchCovidData()
+    this.dataDate = data.Date
+    this.stats = data.Global
+    this.countries = data.Countries
+    this.loading = false
+
+    console.log(this.stats)
+},
+}
+</script>
